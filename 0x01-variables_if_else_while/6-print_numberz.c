112cc6c2e9b51e63da3933e2fa3f923cfa9c7e93
LeyoThio <thioyves@gmail.com>
2022-03-12 05:09:20 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Return: 0 if success
+*/
+int main(void)
+{
+   
+for (int i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+return (0);
+}
